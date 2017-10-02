02b5f60534cab053af5e7af9c417e0ca36ba2ccf
matirivas1995 <matias.rt@hotmail.com>
2017-10-01 22:20:41 -0300

    boton de carrito agregado

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -15,6 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
   producto:any;
+  cantidad:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.producto=this.navParams.get('pro');
   }
