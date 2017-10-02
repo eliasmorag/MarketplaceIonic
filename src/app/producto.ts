451f9146863de70f1f2c61932bb658a59c83a5e2
matirivas1995 <matias.rt@hotmail.com>
2017-10-02 18:57:24 -0300

    modificadas

---
+++ src/app/producto.ts
@@ -0,0 +1,13 @@
+export class Producto {
+    id: number;
+    name: string;
+    descripcion: string;
+    precio: number;
+    foto: string;
+    color: string;
+    tipo: string;
+    cantidad: number;
+    dimensiones: string;
+    peso: number;
+  }
+  
